9203cb94a65090e0842dc090c8e925698168c58c
ameraelbassal552005 <ameraelbassal552005@gmail.com>
2025-05-12 08:35:01 +0000

    finish version 1 (signup)

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ using Microsoft.EntityFrameworkCore;
 using myapp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
