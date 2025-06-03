f495c8c31901b066afdf77ae8ce4bcc11d60667a
Amera <ameraelbassal552005@gmail.com>
2025-06-04 00:37:15 +0300

    add OTP model

---
+++ auth/Models/VerifyOtpRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace myapp.auth.Models
+{
+    public class VerifyOtpRequest
+    {
+        public required string Email { get; set; }
+        public required string Otp { get; set; }
+    }
+}
